fbbfc8dc50b3adf50494fe0b31b68b422a79b998
Borshchick <19archenemy92@gmail.com>
2021-07-23 13:54:35 +0300

    create new repo

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import {BottomNavigation, Tabs} from "@material-ui/core"
+import styles from "./Footer.module.css"
+import {createStyles, makeStyles} from "@material-ui/core/styles"
+import {NavLink} from "react-router-dom"
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        root: {
+            backgroundColor: "rgba(63,65,92,0.75)",
+            margin: "3px 0 0 0"
+        },
+
+    }),
+)
+
+
+export const Footer: React.FC = () => {
+    const classes = useStyles()
+
+    return (
+        <BottomNavigation className={`${classes.root} ${styles.footer}`}>
+                <NavLink to={"/#"}>link</NavLink>
+                <NavLink to={"/#"}>link</NavLink>
+                <NavLink to={"/#"}>link</NavLink>
+                <NavLink to={"/#"}>link</NavLink>
+        </BottomNavigation>
+    )
+}
+
+
