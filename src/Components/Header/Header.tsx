cb9f6c69f56dc9a34ed9742995a68961a95118d6
Borshchick <19archenemy92@gmail.com>
2021-07-26 01:03:36 +0300

    refactor component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,103 +1,41 @@
 import React from "react"
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
-import IconButton from "@material-ui/core/IconButton"
 import Typography from "@material-ui/core/Typography"
-import InputBase from "@material-ui/core/InputBase"
-import {createStyles, alpha, Theme, makeStyles} from "@material-ui/core/styles"
+import IconButton from "@material-ui/core/IconButton"
 import MenuIcon from "@material-ui/icons/Menu"
-import SearchIcon from "@material-ui/icons/Search"
-import styles from "./Header.module.css"
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             flexGrow: 1,
-            margin: "0 0 3px 0",
+            backgroundColor: "black",
+            [theme.breakpoints.up(950)]: {
+                display: "none"
+            },
+            position: "fixed",
+            zIndex: 2,
+            width: "100vw"
         },
         menuButton: {
             marginRight: theme.spacing(2),
         },
-        title: {
-            flexGrow: 1,
-            display: "none",
-            [theme.breakpoints.up("sm")]: {
-                display: "block",
-            },
-        },
-        search: {
-            position: "relative",
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: alpha(theme.palette.common.white, 0.15),
-            "&:hover": {
-                backgroundColor: alpha(theme.palette.common.white, 0.25),
-            },
-            marginLeft: 0,
-            width: "100%",
-            [theme.breakpoints.up("sm")]: {
-                marginLeft: theme.spacing(1),
-                width: "auto",
-            },
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: "100%",
-            position: "absolute",
-            pointerEvents: "none",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        inputRoot: {
-            color: "inherit",
-        },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-            transition: theme.transitions.create("width"),
-            width: "100%",
-            [theme.breakpoints.up("sm")]: {
-                width: "12ch",
-                "&:focus": {
-                    width: "20ch",
-                },
-            },
-        },
     }),
 )
-
 export const Header: React.FC = () => {
     const classes = useStyles()
 
     return (
-        <div className={`${classes.root} ${styles.header}`}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                    >
+        <div className={classes.root} style={{backgroundColor: "black"}}>
+            <AppBar position="static" style={{backgroundColor: "black", borderBottom: "2px solid white"}} >
+                <Toolbar variant="dense">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon/>
                     </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        Portfolio
+                    <Typography variant="h6" color="inherit">
+                        Photos
                     </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon/>
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{"aria-label": "search"}}
-                        />
-                    </div>
                 </Toolbar>
             </AppBar>
         </div>
